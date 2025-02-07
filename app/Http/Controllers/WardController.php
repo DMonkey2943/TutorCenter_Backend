3640e878aad9ddc871aafcdc49a1a9d13e67964d
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-02-07 08:26:39 +0700

    add index of wards

---
+++ app/Http/Controllers/WardController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ward;
+use Illuminate\Http\Request;
+
+class WardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $wards = Ward::all();
+        return response()->json(
+            [
+                'success' => true,
+                'data' => $wards,
+                'message' => 'Wards retrieved successfully'
+            ]
+        );
+    }
+
+    public function getAllBelongToDistrict($id)
+    {
+        $wards =  Ward::where('district_id', $id)->get();
+        return response()->json(
+            [
+                'success' => true,
+                'data' => $wards,
+                'message' => 'Wards belong to district retrieved successfully'
+            ]
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ward $ward)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Ward $ward)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ward $ward)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ward $ward)
+    {
+        //
+    }
+}
